08ec9d0f8d470dae2ed94f0e804137f91eb24163
varayush007 <varayush07@gmail.com>
2024-04-19 05:07:15 +0530

    feat : ChatBot&Semantic_Analysis

---
+++ main.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+import re
+import os
+from langchain.memory import ConversationBufferWindowMemory
+from transformers import pipeline
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+load_dotenv()
+
+
+conversation_with_summary = ConversationChain(
+    llm=OpenAI(temperature=0, model_name="gpt-3.5-turbo"),
+    memory=ConversationBufferWindowMemory(k=2),
+)
+
+sentiment_pipeline = pipeline("sentiment-analysis",
+                              model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+
+
+def main():
+    st.title("Conversational Chatbot")
+
+    if "bot_responses" not in st.session_state:
+        st.session_state.bot_responses = []
+
+    user_input = st.text_input("Ask something to Chat with the bot or type quit to exit")
+
+    if st.button("Send"):
+        if user_input.lower() == 'quit':
+            st.write("Chatbot: Goodbye!")
+        else:
+            raw_response = conversation_with_summary.predict(input=user_input)
+            response = re.split(r'\n|AI:', raw_response)[0].strip()
+            if "Human:" in response:
+                response = response.split("Human:")[0].strip()
+
+            st.session_state.bot_responses.append(response)
+            st.write("Chatbot:", response)
+
+    def perform_sentiment_analysis(answers):
+        elements = []
+        for it in answers:
+            sentiment_result = sentiment_pipeline(it)
+            sentiment_label = sentiment_result[0]['label']
+            elements.append(sentiment_label)
+        return elements
+
+    def visualize_sentiment_analysis(elements):
+        sentiment_counts = {label: elements.count(label) for label in set(elements)}
+
+        labels = sentiment_counts.keys()
+        sizes = sentiment_counts.values()
+        colors = ['green', 'red', 'gray']
+
+        fig, ax = plt.subplots()
+        ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
+        ax.set_title('Sentiment Analysis Results')
+        st.pyplot(fig)
+
+
+    if st.sidebar.button("Perform Sentiment Analysis"):
+        if st.session_state.bot_responses:
+            sentiments = perform_sentiment_analysis(st.session_state.bot_responses)
+            st.write("Sentiments:", sentiments)
+        else:
+            st.write("No bot responses available for sentiment analysis.")
+
+    if st.sidebar.button("Visualize Sentiment Analysis"):
+        if st.session_state.bot_responses:
+            sentiments = perform_sentiment_analysis(st.session_state.bot_responses)
+            visualize_sentiment_analysis(sentiments)
+        else:
+            st.write("No bot responses available for visualization.")
+
+
+if __name__ == "__main__":
+    main()
